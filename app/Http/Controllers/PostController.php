34d49f582a63ab44ad52ef8f736cff6be6a959e4
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-02-27 02:30:19 +0500

    Creare PostController: Posts paginate and Search Paginates

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Http\Requests\PostSearchRequest;
+
+class PostController extends Controller
+{
+    public function index(){
+        $perPage=request()->per_page ?? 10;
+        $posts=Post::paginate( $perPage);
+        return response()->json($posts);
+    }
+    public function search(PostSearchRequest $request)
+    {
+    $searchTerm = $request->get('query', 'Labore');
+    $perPage = $request->get('per_page', 20);
+    $posts=Post::paginate( $perPage);
+    $posts = Post::where('title', 'like', "%$searchTerm%")
+        ->orWhere('content', 'like', "%$searchTerm%")
+        ->paginate($perPage);
+    return response()->json($posts);
+    }
+}
